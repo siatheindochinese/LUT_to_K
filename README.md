2c472731b64ca9690bcbfa5e5301104872cd5109
siatheindochinese <commander_sia@hotmail.com>
2023-04-29 14:46:44 +0800

    added least-squares for Ax=b problem statement

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Recover a camera intrinsic matrix (K) from a corresponding lookup table (LUT) th
 - [x] Load LUT from `.bin` files
 - [x] Generate LUT from K
 - [x] Generate pixel-ray correspondences
+- [x] Formulate problem statement
+- [x] Implement naive solution
+- [ ] LUT sanity check
 - [ ] Random K generator
-- [ ] Formulate problem statement
-- [ ] Implement naive solutions
 - [ ] Implement numerical solutions
---
+++ solver.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def leastSquares(coords_2d, rays_3d):
+	A = (rays_3d / rays_3d[2]).T
+	Ax = A[:,[0,2]]
+	Ay = A[:,[1,2]]
+	
+	b = (coords_2d / coords_2d[2]).T
+	bx = b[:,0]
+	by = b[:,1]
+	
+	# solve for fx and cx
+	fx, cx = np.linalg.inv(Ax.T @ Ax) @ Ax.T @ bx
+	# solve for fy and cy
+	fy, cy = np.linalg.inv(Ax.T @ Ax) @ Ax.T @ bx
+	
+	K = np.eye(3)
+	K[0,0], K[1,1], K[0,2], K[1,2] = fx, fy, cx, cy
+	
+	return K
